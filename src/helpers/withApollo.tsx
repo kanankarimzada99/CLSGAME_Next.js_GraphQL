ccb68298968de50ebdd17cbc6dc3d96fd0e9f27d
Kanan Karizma <kowa9995@gmail.com>
2019-11-28 04:26:11 +0800

    update url

--- src/helpers/withApollo.tsx
+++ src/helpers/withApollo.tsx
@@ -3,10 +3,11 @@ import withApollo from 'next-with-apollo';
 import ApolloClient, { InMemoryCache } from 'apollo-boost';
 // import { GRAPHQL_URL } from '../configs';
 
+// TODO: set url in config
 export default withApollo(
   ({ ctx, headers, initialState }) =>
     new ApolloClient({
-      uri: 'http://localhost:3000/graphql',
+      uri: 'http://ec2-13-59-81-130.us-east-2.compute.amazonaws.com/graphql',
       cache: new InMemoryCache().restore(initialState || {})
     })
 );
